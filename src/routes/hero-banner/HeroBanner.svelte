b9c46145268f86f056f97d3cb40d5a983ca948eb
Sam Hobson <shhobson@protonmail.com>
2023-12-25 17:30:55 +1100

    Added basic hero banner

---
+++ src/routes/hero-banner/HeroBanner.svelte
@@ -0,0 +1,31 @@
+<script>
+
+    import * as left from './banner-left.png';
+    import * as right from './banner-right.png';
+
+</script>
+
+<!-- Banner colour: #ffeae1 -->
+
+<div class="w-screen h-108 flex flex-row">
+
+    <img src={left.default} class="w-auto h-full" />
+
+    <div class="w-full h-full banner-background"></div>
+
+    <img src={right.default} class="w-auto h-full" />
+
+</div>
+
+
+<style>
+
+    .h-108 {
+        height: 27rem;
+    }
+
+    .banner-background {
+        background-color: #ffeae1;
+    }
+
+</style>
